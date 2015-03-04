2490afb1c833586626e9c206eb603b9d790df2bd
Kare Morstol <kare@nottoobadsoftware.com>
2015-03-04 21:29:08 +0100

    Initial commit

---
+++ Drag caret between text views/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drag caret between text views
+//
+//  Created by Kåre Morstøl on 04.03.15.
+//  Copyright (c) 2015 NotTooBad Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
